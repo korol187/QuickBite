d11d55dd966c35237158aed59bdc8ec815b3bf90
Dmytro Korol <korolph@gmail.com>
2025-08-10 16:17:45 +0300

    identity-service base 2

---
+++ services/identity-service/src/auth/auth.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(
+    @Body() registerUserDto: RegisterUserDto,
+  ): Promise<Omit<User, 'password'>> {
+    return this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(
+    @Body() loginUserDto: LoginUserDto,
+  ): Promise<{ access_token: string }> {
+    return this.authService.login(loginUserDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req: { user: User }): Omit<User, 'password'> {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...user } = req.user;
+    return user;
+  }
+}
---
+++ services/identity-service/src/auth/auth.service.ts
@@ -0,0 +1,77 @@
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from './dto/login-user.dto';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(
+    registerUserDto: RegisterUserDto,
+  ): Promise<Omit<User, 'password'>> {
+    const { email, password } = registerUserDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...result } = user;
+    return result;
+  }
+
+  async validateUser(
+    loginUserDto: LoginUserDto,
+  ): Promise<Omit<User, 'password'> | null> {
+    const { email, password } = loginUserDto;
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<{ access_token: string }> {
+    const user = await this.validateUser(loginUserDto);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload: { email: string; sub: string; role: string } = {
+      email: user.email,
+      sub: user.id,
+      role: user.role,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
